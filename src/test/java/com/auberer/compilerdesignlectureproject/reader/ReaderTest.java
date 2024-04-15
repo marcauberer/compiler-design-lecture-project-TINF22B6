3c6fd942844dfb3c60c7431a070049309cbe1864
Jakob Fassunge <87950162+dolerich-hirnfiedler@users.noreply.github.com>
2024-04-15 16:39:41 +0200

    add: Reader.java JUnit Testfile

--- src/test/java/com/auberer/compilerdesignlectureproject/reader/ReaderTest.java
+++ src/test/java/com/auberer/compilerdesignlectureproject/reader/ReaderTest.java
@@ -1,4 +1,14 @@
 package com.auberer.compilerdesignlectureproject.reader;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class ReaderTest {
+    @Test
+    @DisplayName("This is a Test Test")
+    public void test() {
+        System.out.printf("This is a test:%n");
+        Assertions.assertEquals("Test", "Test");
+    }
 }
