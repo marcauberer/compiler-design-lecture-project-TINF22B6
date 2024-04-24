e9d2872c349df22fb73adc5a78d14ac1e32abbc2
Ines Storck <istorck@allegion.com>
2024-04-24 11:48:02 +0200

    Add Switch Parsing

--- src/main/java/com/auberer/compilerdesignlectureproject/ast/ASTCasesNode.java
+++ src/main/java/com/auberer/compilerdesignlectureproject/ast/ASTCasesNode.java
@@ -4,23 +4,13 @@ import com.auberer.compilerdesignlectureproject.lexer.TokenType;
 
 import java.util.Set;
 
-public class ASTCasesNode extends ASTNode {
+public class ASTCasesNode extends ASTNode{
+    @Override
+    public <T> T accept(ASTVisitor<T> visitor) {
+        return visitor.visitCases(this);
+    }
 
-  private int casesSize = 0;
-  @Override
-  public <T> T accept(ASTVisitor<T> visitor) {
-    return visitor.visitCases(this);
-  }
-
-  public static Set<TokenType> getSelectionSet() {
-    return Set.of(TokenType.TOK_CASE);
-  }
-
-  public void setCasesSize(int casesSize){
-    this.casesSize = casesSize;
-  }
-
-  public int getCasesSize() {
-    return casesSize;
-  }
+    public static Set<TokenType> getSelectionSet(){
+        return Set.of(TokenType.TOK_CASE);
+    }
 }
