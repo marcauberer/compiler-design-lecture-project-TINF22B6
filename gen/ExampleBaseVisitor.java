e3ecb700052824a29b58f55db8613e465822f798
Schwinghammer, Max <max.schwinghammer@vaillant-group.com>
2024-05-08 11:25:09 +0200

    Added visitForLoop() method and generated files

---
+++ gen/ExampleBaseVisitor.java
@@ -0,0 +1,28 @@
+// Generated from C:/IdeaProjects/compiler-design-lecture-project-TINF22B6/Example.g4 by ANTLR 4.13.1
+import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
+
+/**
+ * This class provides an empty implementation of {@link ExampleVisitor},
+ * which can be extended to create a visitor which only needs to handle a subset
+ * of the available methods.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+@SuppressWarnings("CheckReturnValue")
+public class ExampleBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements ExampleVisitor<T> {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitAddition(ExampleParser.AdditionContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitConst(ExampleParser.ConstContext ctx) { return visitChildren(ctx); }
+}
