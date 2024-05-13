6e8ada57536c5fe13461959ed07641edf7f628b2
MargotGeisbauer <margot.geisbauer@esentri.com>
2024-05-13 21:12:25 +0200

    gruppe 8

--- src/main/java/com/auberer/compilerdesignlectureproject/ast/ASTAdditiveExprNode.java
+++ src/main/java/com/auberer/compilerdesignlectureproject/ast/ASTAdditiveExprNode.java
@@ -1,16 +1,36 @@
 package com.auberer.compilerdesignlectureproject.ast;
 
+import com.auberer.compilerdesignlectureproject.lexer.Token;
 import com.auberer.compilerdesignlectureproject.lexer.TokenType;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class ASTAdditiveExprNode extends ASTNode{
+
+    public static List<AdditiveOperator> operatorList = new ArrayList<AdditiveOperator>();
+
+    public static void operatorsListAdd(AdditiveOperator a){
+        operatorList.add(a);
+    }
+
+    public List<AdditiveOperator> operatorsList = new ArrayList<>();
+
     @Override
     public <T> T accept(ASTVisitor<T> visitor) {
         return visitor.visitAdditiveExpr(this);
     }
 
     public static Set<TokenType> getSelectionSet() {
-      return ASTCompareExprNode.getSelectionSet();
+        Set<TokenType> selectionSet = new HashSet<>();
+        selectionSet.addAll(ASTPrefixExprNode.getSelectionSet());
+        return selectionSet;
+    }
+
+    public enum AdditiveOperator {
+        PLUS,
+        MINUS
     }
 }
--- src/main/java/com/auberer/compilerdesignlectureproject/ast/ASTMultiplicativeExprNode.java
+++ src/main/java/com/auberer/compilerdesignlectureproject/ast/ASTMultiplicativeExprNode.java
@@ -2,15 +2,32 @@ package com.auberer.compilerdesignlectureproject.ast;
 
 import com.auberer.compilerdesignlectureproject.lexer.TokenType;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class ASTMultiplicativeExprNode extends ASTNode{
+
+    public static List<MultiplicativeOperator> operatorList = new ArrayList<MultiplicativeOperator>();
+
+    public static void operatorsListAdd(MultiplicativeOperator a){
+        operatorList.add(a);
+    }
+
     @Override
     public <T> T accept(ASTVisitor<T> visitor) {
         return visitor.visitMultiplicativeExpr(this);
     }
 
     public static Set<TokenType> getSelectionSet() {
-      return ASTCompareExprNode.getSelectionSet();
+        Set<TokenType> selectionSet = new HashSet<>();
+        selectionSet.addAll(ASTPrefixExprNode.getSelectionSet());
+        return selectionSet;
+    }
+
+    public enum MultiplicativeOperator {
+        MUL,
+        DIV
     }
 }
