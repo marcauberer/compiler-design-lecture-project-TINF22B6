e2aed138488ddcffc22380838f11ba67546d5281
Karim <122603912+mirak55@users.noreply.github.com>
2024-05-21 12:19:14 +0200

    Team7 (#40)

--- src/main/java/com/auberer/compilerdesignlectureproject/ast/ASTVarDeclNode.java
+++ src/main/java/com/auberer/compilerdesignlectureproject/ast/ASTVarDeclNode.java
@@ -4,9 +4,10 @@ import com.auberer.compilerdesignlectureproject.lexer.TokenType;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.Set;
 
-public class ASTVarDeclNode extends ASTNode{
+public class ASTVarDeclNode extends ASTNode {
 
     @Getter
     @Setter
@@ -18,6 +19,17 @@ public class ASTVarDeclNode extends ASTNode{
     }
 
     public static Set<TokenType> getSelectionSet() {
-        return  ASTTypeNode.getSelectionSet();
+
+        return ASTTypeNode.getSelectionSet();
+    }
+
+    public ASTTypeNode getType() {
+
+        return getChild(ASTTypeNode.class, 0);
+    }
+
+    public ASTAssignStmtNode getAssign() {
+
+        return getChild(ASTAssignStmtNode.class, 0);
     }
 }
--- src/test/java/com/auberer/compilerdesignlectureproject/parser/AssignStmtNodeTest.java
+++ src/test/java/com/auberer/compilerdesignlectureproject/parser/AssignStmtNodeTest.java
@@ -1,10 +1,12 @@
 package com.auberer.compilerdesignlectureproject.parser;
 
 import com.auberer.compilerdesignlectureproject.ast.ASTAssignStmtNode;
+import com.auberer.compilerdesignlectureproject.ast.ASTStmtNode;
 import com.auberer.compilerdesignlectureproject.lexer.Lexer;
 import com.auberer.compilerdesignlectureproject.lexer.Token;
 import com.auberer.compilerdesignlectureproject.lexer.TokenType;
 import com.auberer.compilerdesignlectureproject.reader.CodeLoc;
+import com.auberer.compilerdesignlectureproject.reader.Reader;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -75,5 +77,20 @@ public class AssignStmtNodeTest {
         assertInstanceOf(ASTAssignStmtNode.class, assignStmt);
         // Ensure the variable name is correct
         assertEquals("xyz", assignStmt.getVariableName());
+    }
+
+    @Test
+    @DisplayName("Integration test")
+    void integrationTestForStatementNode() {
+        String code = "x = 5;";
+        Reader reader = new Reader(code);
+        Lexer lexer = new Lexer(reader, false);
+        Parser parser = new Parser(lexer);
+        ASTAssignStmtNode astStmtNode = parser.parseAssignStmt();
+
+        assertNotNull(astStmtNode);
+        assertInstanceOf(ASTAssignStmtNode.class, astStmtNode);
+        assertNotNull(astStmtNode.getLogical());
+        assertInstanceOf(ASTAssignStmtNode.class, astStmtNode.getLogical());
     }
 }
