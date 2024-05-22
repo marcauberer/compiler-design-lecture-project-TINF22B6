9dd3dfc94d95d3c8bf59462e72a80ed3f4b9f287
tech-n1c <nicimaiwald@gmail.com>
2024-05-22 11:06:51 +0200

    Added visitForLoop to SymbolTableBuilder

--- src/main/java/com/auberer/compilerdesignlectureproject/sema/SymbolTableBuilder.java
+++ src/main/java/com/auberer/compilerdesignlectureproject/sema/SymbolTableBuilder.java
@@ -1,6 +1,7 @@
 package com.auberer.compilerdesignlectureproject.sema;
 
 import com.auberer.compilerdesignlectureproject.ast.ASTEntryNode;
+import com.auberer.compilerdesignlectureproject.ast.ASTForNode;
 import com.auberer.compilerdesignlectureproject.ast.ASTVarDeclNode;
 import com.auberer.compilerdesignlectureproject.ast.ASTVisitor;
 
@@ -9,6 +10,26 @@ import java.util.Stack;
 public class SymbolTableBuilder extends ASTVisitor<Void> {
 
   Stack<Scope> currentScopes = new Stack<>();
+
+  @Override
+  public Void visitForLoop(ASTForNode node) {
+    Scope forScope = new Scope();
+    currentScopes.push(forScope);
+
+    visitChildren(node);
+
+    // Check if the initialization, condition, and increment are not null
+    if (node.getInitialization() == null)
+      throw new SemaError("For loop missing initialization");
+    if (node.getCondition() == null)
+        throw new SemaError("For loop missing condition");
+    if (node.getIncrement() == null)
+        throw new SemaError("For loop missing increment");
+
+    assert currentScopes.size() > 1 && currentScopes.peek() == forScope;
+    currentScopes.pop();
+    return null;
+  }
 
   @Override
   public Void visitEntry(ASTEntryNode node) {
