2596e5461efd232b04cd97a58d975a1d564ad34e
Marc Auberer <marc.auberer@sap.com>
2024-05-22 11:26:22 +0200

    Create root scope in SymbolTableBuilder ctor

--- src/main/java/com/auberer/compilerdesignlectureproject/sema/SymbolTableBuilder.java
+++ src/main/java/com/auberer/compilerdesignlectureproject/sema/SymbolTableBuilder.java
@@ -1,6 +1,7 @@
 package com.auberer.compilerdesignlectureproject.sema;
 
 import com.auberer.compilerdesignlectureproject.ast.ASTEntryNode;
+import com.auberer.compilerdesignlectureproject.ast.ASTFctDefNode;
 import com.auberer.compilerdesignlectureproject.ast.ASTVarDeclNode;
 import com.auberer.compilerdesignlectureproject.ast.ASTVisitor;
 
@@ -10,19 +11,19 @@ public class SymbolTableBuilder extends ASTVisitor<Void> {
 
   Stack<Scope> currentScopes = new Stack<>();
 
+  public SymbolTableBuilder() {
+    assert currentScopes.empty();
+    currentScopes.push(new Scope());
+  }
+
   @Override
   public Void visitEntry(ASTEntryNode node) {
-    Scope rootScope = new Scope();
-    currentScopes.push(rootScope);
-
     visitChildren(node);
 
     // Check if main function is present
-    if (rootScope.lookupSymbol("main") == null)
+    if (currentScopes.peek().lookupSymbol("main") == null)
       throw new SemaError("No main function found");
 
-    assert currentScopes.size() == 1 && currentScopes.peek() == rootScope;
-    currentScopes.pop();
     return null;
   }
 }
