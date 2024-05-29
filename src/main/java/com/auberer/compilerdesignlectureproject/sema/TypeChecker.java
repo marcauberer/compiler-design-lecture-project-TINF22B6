1d91b9901a7b4f3f7ce1a2c0661e16392c6d6953
Ines Storck <storck.ines@gmail.com>
2024-05-29 12:41:15 +0200

    TypeChecker

--- src/main/java/com/auberer/compilerdesignlectureproject/sema/TypeChecker.java
+++ src/main/java/com/auberer/compilerdesignlectureproject/sema/TypeChecker.java
@@ -1,7 +1,6 @@
 package com.auberer.compilerdesignlectureproject.sema;
 
-import com.auberer.compilerdesignlectureproject.ast.ASTPrintBuiltinCallNode;
-import com.auberer.compilerdesignlectureproject.ast.ASTVisitor;
+import com.auberer.compilerdesignlectureproject.ast.*;
 
 import java.util.Stack;
 
@@ -19,6 +18,38 @@ public class TypeChecker extends ASTVisitor<ExprResult> {
     visitChildren(node);
 
     // ToDo: Insert check if logicalExpr is int, double or string
+
+    Type resultType = new Type(SuperType.TY_EMPTY);
+    return new ExprResult(node.setEvaluatedSymbolType(resultType));
+  }
+
+  @Override
+  public ExprResult visitSwitchStmt(ASTSwitchStmtNode node) {
+    ASTLogicalExprNode logicalExprNode = node.getLogicalExpr();
+    ExprResult result = visit(logicalExprNode);
+    if(!result.getType().isOneOf(SuperType.TY_INT, SuperType.TY_DOUBLE, SuperType.TY_STRING)){
+      throw new SemaError(node, "Switch statement expects int, double or string, but got '" + result.getType().toString() + "'");
+    }
+
+    node.getCases().setExpectedType(result.getType());
+
+    visit(node.getCases());
+    visit(node.getDefault());
+
+    Type resultType = new Type(SuperType.TY_EMPTY);
+    return new ExprResult(node.setEvaluatedSymbolType(resultType));
+  }
+
+  @Override
+  public ExprResult visitCases(ASTCasesNode node) {
+    node.getExpectedType();
+
+    return super.visitCases(node);
+  }
+
+  @Override
+  public ExprResult visitDefault(ASTDefaultNode node) {
+    visitChildren(node);
 
     Type resultType = new Type(SuperType.TY_EMPTY);
     return new ExprResult(node.setEvaluatedSymbolType(resultType));
