72007b0bbb8ae06e13313057bbe3b0746493a3a1
Jakob Fassunge <87950162+dolerich-hirnfiedler@users.noreply.github.com>
2024-06-07 15:48:53 +0200

    som formatting

--- src/test/java/com/auberer/compilerdesignlectureproject/codegen/IfStmtIRGeneratorTest.java
+++ src/test/java/com/auberer/compilerdesignlectureproject/codegen/IfStmtIRGeneratorTest.java
@@ -2,6 +2,7 @@ package com.auberer.compilerdesignlectureproject.codegen;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.auberer.compilerdesignlectureproject.ast.ASTIfStmtNode;
 import com.auberer.compilerdesignlectureproject.lexer.Lexer;
 import com.auberer.compilerdesignlectureproject.parser.Parser;
 import com.auberer.compilerdesignlectureproject.reader.Reader;
@@ -11,31 +12,33 @@ import org.junit.jupiter.api.Test;
 
 public class IfStmtIRGeneratorTest {
 
-  @Test
-  @DisplayName("Dump IR")
-  public void dumpEmptyModule() {
-    // Create a new Reader object with the given file path
-    String input = """
-        if (true == false) {
-            print("XSLT");
-        }
-        """;
+    @Test
+    @DisplayName("Dump IR")
+    public void dumpEmptyModule() {
+        // Create a new Reader object with the given file path
+        String input = """
+                if (true == false) {
+                    print("XSLT");
+                }
+                """;
 
-    Reader reader = new Reader(input);
-    Lexer lexer = new Lexer(reader, false);
-    Parser parser = new Parser(lexer);
+        Reader reader = new Reader(input);
+        Lexer lexer = new Lexer(reader, false);
+        Parser parser = new Parser(lexer);
+        ASTIfStmtNode ast = parser.parseIfStmt();
 
-    BasicBlock startBlock = new BasicBlock("root");
-    IRGenerator irGenerator = new IRGenerator("ifStatement");
-    irGenerator.setCurrentBlock(startBlock);
-    Function function = new Function("ifStatement");
-    function.setEntryBlock(startBlock);
-    StringBuilder sb = new StringBuilder();
-    function.dumpIR(sb);
+        BasicBlock startBlock = new BasicBlock("root");
+        IRGenerator irGenerator = new IRGenerator("ifStatement");
+        irGenerator.setCurrentBlock(startBlock);
+        irGenerator.visitIf(ast);
+        Function function = new Function("ifStatement");
+        function.setEntryBlock(startBlock);
+        StringBuilder sb = new StringBuilder();
+        function.dumpIR(sb);
 
-    System.out.printf("%s%n", sb.toString());
+        System.out.printf("%s%n", sb.toString());
 
-    assertEquals(sb.toString().trim(), "function ifStatement: {\n}");
-  }
+        assertEquals(sb.toString().trim(), "function ifStatement: {\n}");
+    }
 
 }
