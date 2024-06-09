0d3befdc935c3c96b11aac5a854a5cd4371b1017
nils bohland <nbo@itscope.de>
2024-06-09 21:18:07 +0200

    changed visitFctDef function to reflect the block-structure Also fixed minor bugs from last week

--- src/main/java/com/auberer/compilerdesignlectureproject/codegen/IRGenerator.java
+++ src/main/java/com/auberer/compilerdesignlectureproject/codegen/IRGenerator.java
@@ -1,21 +1,52 @@
 package com.auberer.compilerdesignlectureproject.codegen;
 
 import com.auberer.compilerdesignlectureproject.ast.*;
-import com.auberer.compilerdesignlectureproject.codegen.instructions.CallInstruction;
-import com.auberer.compilerdesignlectureproject.codegen.instructions.Instruction;
-import com.auberer.compilerdesignlectureproject.codegen.instructions.PrintInstruction;
+import com.auberer.compilerdesignlectureproject.codegen.instructions.*;
 import lombok.Getter;
-
+import lombok.Setter;
+@Getter
 public class IRGenerator extends ASTVisitor<IRExprResult> {
 
   // IR module, which represents the whole program
-  @Getter
+
   private final Module module;
   // The basic block, which is currently the insert point for new instructions
+  @Setter
   private BasicBlock currentBlock = null;
 
   public IRGenerator(String moduleName) {
     module = new Module(moduleName);
+  }
+
+
+  @Override
+  public IRExprResult visitForLoop(ASTForNode node) {
+    BasicBlock loopHeadBlock = new BasicBlock("loopHead");
+    BasicBlock loopBodyBlock = new BasicBlock("loopBody");
+    BasicBlock loopEndBlock = new BasicBlock("loopEnd");
+
+    visit(node.getInitialization());
+
+    JumpInstruction jumpToLoopHead = new JumpInstruction(node, loopHeadBlock);
+    pushToCurrentBlock(jumpToLoopHead);
+
+    switchToBlock(loopHeadBlock);
+    IRExprResult conditionResult = visit(node.getCondition());
+
+    CondJumpInstruction condJumpInstruction = new CondJumpInstruction(node, conditionResult.getValue().getNode(), loopBodyBlock, loopEndBlock);
+    pushToCurrentBlock(condJumpInstruction);
+
+    switchToBlock(loopBodyBlock);
+    visit(node.getBody());
+
+    visit(node.getIncrement());
+
+    JumpInstruction jumpInstruction = new JumpInstruction(node, loopHeadBlock);
+    pushToCurrentBlock(jumpInstruction);
+
+    switchToBlock(loopEndBlock);
+
+    return null;
   }
 
   @Override
@@ -39,8 +70,48 @@ public class IRGenerator extends ASTVisitor<IRExprResult> {
     return new IRExprResult(null, node, null);
   }
 
-  // ToDo: Insert other visit methods here
 
+
+  @Override
+  public IRExprResult visitWhileLoop(ASTWhileLoopNode node) {
+    BasicBlock conditionBlock = new BasicBlock("while.cond");
+    BasicBlock bodyBlock = new BasicBlock("while.body");
+    BasicBlock exitBlock = new BasicBlock("while.exit");
+
+    JumpInstruction jump = new JumpInstruction(node, conditionBlock);
+    pushToCurrentBlock(jump);
+
+    switchToBlock(conditionBlock);
+    visitLogicalExpr(node.getCondition());
+    CondJumpInstruction condJump = new CondJumpInstruction(node, node.getCondition(), bodyBlock, exitBlock);
+    pushToCurrentBlock(condJump);
+    switchToBlock(bodyBlock);
+
+    visitStmtLst(node.getBody());
+    jump = new JumpInstruction(node, conditionBlock);
+    pushToCurrentBlock(jump);
+    switchToBlock(conditionBlock);
+
+    switchToBlock(exitBlock);
+    return new IRExprResult(null, node, null);
+  }
+
+  @Override
+  public IRExprResult visitDoWhileLoop(ASTDoWhileLoopNode node) {
+    BasicBlock doWhileBlock = new BasicBlock("do_while.body");
+    BasicBlock endDoWhileBlock = new BasicBlock("do_while.exit");
+    CondJumpInstruction condJumpInstruction = new CondJumpInstruction(node, node.getCondition(), doWhileBlock, endDoWhileBlock);
+
+    JumpInstruction jumpInstruction = new JumpInstruction(node, doWhileBlock);
+    currentBlock.pushInstruction(jumpInstruction);
+
+    switchToBlock(doWhileBlock);
+    visitStmtLst(node.getBody());
+    doWhileBlock.pushInstruction(condJumpInstruction);
+    switchToBlock(endDoWhileBlock);
+
+    return new IRExprResult(null, node, null);
+  }
   /**
    * Can be used to set the instruction insert point to a specific block
    *
@@ -60,7 +131,7 @@ public class IRGenerator extends ASTVisitor<IRExprResult> {
    */
   private void finalizeFunction() {
     assert currentBlock != null;
-    assert isBlockTerminated(currentBlock);
+    assert !isBlockTerminated(currentBlock);
     currentBlock = null;
   }
 
